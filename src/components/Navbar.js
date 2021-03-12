53fa42c41d61dc7ec88ad34231b96a9c4e5b588d
Yohan Fleury <yfleury95@gmail.com>
2021-03-12 15:57:48 +0100

    done : navbar & footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <header>
-            <div>
-                
+            <div className="d-flex flex-column flex-md-row p-3 border-bottom bg-secondary text-white">
+                <h4 className="mr-md-auto">
+                    <a href="/" className="text-decoration-none text-white"> BOOKS</a>
+                </h4>
             </div>
         </header>
     )
